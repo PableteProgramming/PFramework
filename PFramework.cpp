a9ed2242b22caf5a870f3f87b64dbb85f7344ac5
PableteProgramming <pabletegym@icloud.com>
2021-06-10 20:43:19 +0200

    Adding terminal apearance with commands vector

--- PFramework.cpp
+++ PFramework.cpp
@@ -1,6 +1,44 @@
 #include <PFramework.hpp>
 
 int main(){
+	while (true){
+		std::string message;
+		std::cout<<"PFramework > ";
+		std::getline(std::cin,message);
+		if (message!=""){
+			std::vector<std::string> r=  Split(message,' ');
+			std::string cmd=r[0];
+			for (int i=0; i< scripts.size();i++){
+				if(scripts[i].first.first==cmd){
+					std::string params="";
+					for(int j=1 ;j<r.size();j++){
+						params+= r[j]+" ";
+					}
+					scripts[i].first.second(params);
+					break;
+				}
+			}
+		}
+	}
 	echo("Hi");
 	return 0;
+}
+
+std::vector<std::string> Split(std::string s,char del){
+	std::vector<std::string> r;
+	r.clear();
+	std::string temp="";
+	for(int i=0; i<s.size();i++){
+		if(s[i]!=del){
+			temp+=s[i];
+		}
+		else{
+			r.push_back(temp);
+			temp="";
+		}
+	}
+	if (temp!=""){
+		r.push_back(temp);
+	}
+	return r;
 }
--- PFramework.hpp
+++ PFramework.hpp
@@ -1,1 +1,10 @@
 #include <echo.hpp>
+#include <vector>
+
+typedef bool (*funcPtr)(std::string);
+
+std::vector<std::string> Split(std::string,char);
+
+std::vector<std::pair<std::pair<std::string,funcPtr>,std::string>> scripts= {
+	{std::make_pair(std::make_pair("echo",&echo),"echo.txt")}
+};
